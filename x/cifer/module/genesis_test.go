a9d1af4ffeb5b2a23bfbe9f39cb7d512053e041a
Developer Experience team at Ignite <hello@ignite.com>
2025-07-09 10:27:18 +0700

    Initialized with Ignite CLI

---
+++ x/cifer/module/genesis_test.go
@@ -0,0 +1,29 @@
+package cifer_test
+
+import (
+	"testing"
+
+	keepertest "cifer/testutil/keeper"
+	"cifer/testutil/nullify"
+	"cifer/x/cifer/module"
+	"cifer/x/cifer/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.CiferKeeper(t)
+	cifer.InitGenesis(ctx, k, genesisState)
+	got := cifer.ExportGenesis(ctx, k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
